2fbf94d6d1fc4bb6a1e96edd040d49328e837c2d
estate-mike <mike.fantat@gmail.com>
2020-08-16 23:35:07 -0700

    Smaller header margins

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ import Button from '../Button';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
-    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
+    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto px-8">
       <div className="flex items-center text-2xl">
         <div className="w-20 mr-3">
           <img src="/pictures/rent-judge-144.png" alt="logo" />

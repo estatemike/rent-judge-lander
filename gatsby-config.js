fc27c970bf2ca9d84635779612a151d7ffd7e28d
estate-mike <mike.fantat@gmail.com>
2020-08-16 23:26:34 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
       options: {
         tailwindConfig: `tailwind.config.js`
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-175548821-1'
+      }
     }
   ]
 };
